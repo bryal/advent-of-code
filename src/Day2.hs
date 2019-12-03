ea16b5b3dc6b2b06439ef88b0f23e4bcbcf385b7
JoJo <jo@jo.zone>
2019-12-03 13:32:51 +0100

    Day 2: Refactor a bit
    Remove some wrapper types & improve how instructions and data is loaded

--- src/Day2.hs
+++ src/Day2.hs
@@ -6,24 +6,16 @@ import Control.Monad.State
 import Control.Applicative
 import Data.Functor
 import Data.List.Split
-import Lens.Micro.Platform (makeLenses, use, to, modifying)
+import Lens.Micro.Platform (makeLenses, use, to, modifying, (<<%=))
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 
-newtype Pos = Pos Word deriving (Ord, Eq)
+newtype Addr = Addr Word deriving (Ord, Eq)
 
-data Instr
-    = Add Pos Pos Pos
-    | Mul Pos Pos Pos
-    | Halt
+type Pc = Word
+type Mem = Seq Word
 
-newtype Pc = Pc { _unPc :: Word }
-makeLenses ''Pc
-
-newtype Mem = Mem { _unMem :: Seq Word }
-makeLenses ''Mem
-
-data St = St { _programCount :: Pc, _memory :: Mem }
+data St = St { _pc :: Pc, _mem :: Mem }
 makeLenses ''St
 
 type Eval a = State St a
@@ -37,11 +29,11 @@ readInput :: IO String
 readInput = readFile "inputs/day-2"
 
 parse :: String -> Mem
-parse = Mem . Seq.fromList . fmap read . splitOn ","
+parse = Seq.fromList . fmap read . splitOn ","
 
 runWithArgs :: Word -> Word -> Mem -> Word
 runWithArgs a b =
-    evalState (store (Pos 1) a >> store (Pos 2) b >> eval) . St (Pc 0)
+    evalState (store (Addr 1) a >> store (Addr 2) b >> eval) . St 0
 
 eval :: Eval Word
 eval = step >>= \case
@@ -49,37 +41,25 @@ eval = step >>= \case
     Nothing -> eval
 
 step :: Eval (Maybe Word)
-step = do
-    nextInstr >>= \case
-        Add i j dst -> (store dst =<< liftA2 (+) (load i) (load j)) $> Nothing
-        Mul i j dst -> (store dst =<< liftA2 (*) (load i) (load j)) $> Nothing
-        Halt -> fmap Just (load (Pos 0))
-
-nextInstr :: Eval Instr
-nextInstr = do
-    St (Pc pc) (Mem mem) <- get
-    let pc' = fromIntegral pc
-    case (Seq.index mem pc') of
-        1 -> binop Add pc' mem
-        2 -> binop Mul pc' mem
-        99 -> incrPc 1 $> Halt
-        _ -> error "Undefined opcode"
+step = next >>= \case
+    1 -> binop (+)
+    2 -> binop (*)
+    99 -> fmap Just (load (Addr 0))
+    _ -> error "Undefined opcode"
   where
-    binop f pc mem =
-        let
-            i = Pos (Seq.index mem (pc + 1))
-            j = Pos (Seq.index mem (pc + 2))
-            dst = Pos (Seq.index mem (pc + 3))
-        in incrPc 4 $> f i j dst
+    next = load . Addr =<< (pc <<%= (+ 1))
+    binop f = do
+        v <- liftA2 f getArg getArg
+        dst <- nextAddr
+        store dst v $> Nothing
+    getArg = load =<< nextAddr
+    nextAddr = fmap Addr next
 
-incrPc :: Word -> Eval ()
-incrPc = modifying (programCount . unPc) . (+)
+store :: Addr -> Word -> Eval ()
+store (Addr i) = modifying mem . Seq.update (fromIntegral i)
 
-store :: Pos -> Word -> Eval ()
-store (Pos i) = modifying (memory . unMem) . Seq.update (fromIntegral i)
-
-load :: Pos -> Eval Word
-load (Pos i) = use (memory . unMem . to (flip Seq.index (fromIntegral i)))
+load :: Addr -> Eval Word
+load (Addr i) = use (mem . to (flip Seq.index (fromIntegral i)))
 
 -- Part 2
 
@@ -87,10 +67,10 @@ part2 :: IO Word
 part2 = fmap part2' readInput where part2' = findNounVerb . parse
 
 findNounVerb :: Mem -> Word
-findNounVerb mem = head $ do
+findNounVerb m = head $ do
     noun <- [0 .. 99]
     verb <- [0 .. 99]
-    if runWithArgs noun verb mem == expectedOutput
+    if runWithArgs noun verb m == expectedOutput
         then [100 * noun + verb]
         else []
 
