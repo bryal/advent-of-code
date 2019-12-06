fa4ef0d1c5512eef8166de504bc575b78787d781
JoJo <jo@jo.zone>
2019-12-06 15:42:00 +0100

    Solve day 5

---
+++ src/Day5.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE LambdaCase, TemplateHaskell, TypeApplications #-}
+
+module Day5 (part1, part2) where
+
+import Control.Monad.State
+import Control.Applicative
+import Data.List.Split
+import Lens.Micro.Platform (makeLenses, use, to, modifying, (<<%=), assign)
+import Data.Sequence (Seq)
+import Data.Composition
+import qualified Data.Sequence as Seq
+
+newtype Addr = Addr Int deriving (Ord, Eq)
+
+type Pc = Int
+type Mem = Seq Int
+
+data St = St { _pc :: Pc, _mem :: Mem }
+makeLenses ''St
+
+type Eval a = StateT St IO a
+
+part1 :: IO Int
+part1 = do
+    putStrLn "Enter \"1\" at first prompt"
+    (run . parse) =<< readInput
+
+part2 :: IO Int
+part2 = do
+    putStrLn "Enter \"5\" at first prompt"
+    (run . parse) =<< readInput
+
+readInput :: IO String
+readInput = readFile "inputs/day-5"
+
+parse :: String -> Mem
+parse = Seq.fromList . fmap read . splitOn ","
+
+run :: Mem -> IO Int
+run = evalStateT eval . St 0
+
+eval :: Eval Int
+eval = step >>= \case
+    Just x -> pure x
+    Nothing -> eval
+
+step :: Eval (Maybe Int)
+step = do
+    (opcode, modes) <- nextInstr
+    let binop' = binop (modes !! 0) (modes !! 1)
+    let jmpIf' = jmpIf (modes !! 0) (modes !! 1)
+    case opcode of
+        1 -> binop' (+)
+        2 -> binop' (*)
+        3 -> input
+        4 -> output (modes !! 0)
+        5 -> jmpIf' (/= 0)
+        6 -> jmpIf' (== 0)
+        7 -> binop' (fromEnum .* (<))
+        8 -> binop' (fromEnum .* (==))
+        99 -> pure ()
+        _ -> error "Undefined opcode"
+    if opcode == 99 then fmap Just (load (Addr 0)) else pure Nothing
+  where
+    nextInstr = fmap (\x -> (extractOpcode x, extractModes x)) next
+    extractOpcode x = mod x 100
+    extractModes x = map (\n -> mod (div x (10 ^ n)) 10) [2 :: Int ..]
+    next = load . Addr =<< (pc <<%= (+ 1))
+    binop amode bmode f = do
+        v <- liftA2 f (getArg amode) (getArg bmode)
+        dst <- nextAddr
+        store dst v
+    input = do
+        v <- lift (putStr "input> " >> fmap (read @Int) getLine)
+        dst <- nextAddr
+        store dst v
+    output mode =
+        getArg mode >>= \x -> lift (putStrLn ("output: " ++ show x))
+    jmpIf amode bmode pred = do
+        (a, b) <- liftA2 (,) (getArg amode) (getArg bmode)
+        when (pred a) (assign pc b)
+    getArg = \case
+        0 -> load =<< nextAddr -- Address mode
+        1 -> next -- Immediate mode
+        _ -> error "Undefined mode"
+    nextAddr = fmap Addr next
+
+store :: Addr -> Int -> Eval ()
+store (Addr i) = modifying mem . Seq.update i
+
+load :: Addr -> Eval Int
+load (Addr i) = use (mem . to (flip Seq.index i))
