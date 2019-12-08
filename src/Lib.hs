e1573cdf46b7d2395869c67b9b4303c783e8b195
JoJo <jo@jo.zone>
2019-12-08 13:32:32 +0100

    Solve day 8

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,13 @@
-module Lib (both, head2) where
+module Lib (both, head2, minimumOn) where
 
 import Data.Bifunctor
+import Data.List
 
 both :: Bifunctor p => (a -> b) -> p a a -> p b b
 both f = bimap f f
 
 head2 :: [a] -> (a, a)
 head2 xs = (head xs, head (tail xs))
+
+minimumOn :: Ord b => (a -> b) -> [a] -> a
+minimumOn f = minimumBy (\a1 a2 -> compare (f a1) (f a2))
