6541074fa2addd870a692b97ca62f573634566fa
JoJo <jo@jo.zone>
2019-12-11 14:40:33 +0100

    Solve day 10

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (both, head2, minimumOn) where
+module Lib (both, head2, minimumOn, maximumOn) where
 
 import Data.Bifunctor
 import Data.List
@@ -11,3 +11,6 @@ head2 xs = (head xs, head (tail xs))
 
 minimumOn :: Ord b => (a -> b) -> [a] -> a
 minimumOn f = minimumBy (\a1 a2 -> compare (f a1) (f a2))
+
+maximumOn :: Ord b => (a -> b) -> [a] -> a
+maximumOn f = maximumBy (\a1 a2 -> compare (f a1) (f a2))
