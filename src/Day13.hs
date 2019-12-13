fad00adfcbd0c6f19d9416531665498da22cd3cd
JoJo <jo@jo.zone>
2019-12-13 22:51:17 +0100

    naming

--- src/Day13.hs
+++ src/Day13.hs
@@ -26,20 +26,19 @@ part2 = fmap (beatGame . parseIntcode) readInput
   where
     beatGame pgm =
         let
-            os = evalIntcode is (insertCoins pgm)
+            outs = evalIntcode ins (insertCoins pgm)
             insertCoins = Seq.update 0 2
-            tiles = map head3 (chunksOf 3 os)
-            (score, is) = play tiles
-            play = play' (0, 0, 0)
-            play' (bx, px, s) = \case
-                (-1, 0, s') : ts -> play' (bx, px, s') ts
-                (px', _, 3) : ts -> play' (bx, px', s) ts
+            tiles = map head3 (chunksOf 3 outs)
+            (score, ins) = play (0, 0, 0) tiles
+            play (bx, px, s) = \case
+                (-1, 0, s') : ts -> play (bx, px, s') ts
+                (px', _, 3) : ts -> play (bx, px', s) ts
                 (bx', _, 4) : ts ->
                     let
                         i = calcMove bx' px
-                        result = play' (bx', px, s) ts
+                        result = play (bx', px, s) ts
                     in second (i :) result
-                _ : ts -> play' (bx, px, s) ts
+                _ : ts -> play (bx, px, s) ts
                 [] -> (s, [])
             calcMove bx px =
                 if bx > px then 1 else if bx == px then 0 else (-1)
